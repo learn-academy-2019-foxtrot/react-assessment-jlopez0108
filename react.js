952243d30d0835b79558d4039d0487c79ae61941
Ubuntu <ubuntu@ip-172-31-17-145.us-west-2.compute.internal>
2019-12-16 16:34:04 +0000

    completed assessment

--- react.js
+++ react.js
@@ -7,61 +7,96 @@
 
 
 
+// import React, { Component } from 'react';
+
+// class App extends Component{
+//   render(){
+//     return (
+//       <h1>I am a component!</h1>
+//     );
+//   }
+// }
+
+// export default App;
+
+
 
 
 // 2. Refactor this vanilla javascript loop to a map function. The output should remain the same.
 
 var names = ["Ford Prefect", "Arthur Dent", "Trillian", "Zaphod", "Vogon", "Marvin, the Paranoid Android"]
 
-for(let i=0; i<names.length; i++){
-  console.log(`${names[i]} is ${names[i].length} characters long.`)
+// for(let i=0; i<names.length; i++){
+//   console.log(`${names[i]} is ${names[i].length} characters long.`)
+// }
+
+names.map((value,index) => { 
+console.log(`${names[index]} is ${names[index].length} characters long.`)
+})
+
+// I tried to have the same output within a function
+
+const hitchHickersGuide = (array) =>{
+  let newArr = array.map((value, index) => console.log(value + " is  "+  value.length + " characters long. "))
+    return newArr
 }
 
+hitchHickersGuide(names)
 
 
 // 3. Destructure the following variables out of state.
 
-this.state = {
-  name: "Dracula",
-  home: "Transylvania",
-  dislikes: ["mirrors", "garlic", "wooden stakes"]
-}
+// this.state = {
+//   name: "Dracula",
+//   home: "Transylvania",
+//   dislikes: ["mirrors", "garlic", "wooden stakes"]
+// }
 
-
+// render() {
+//   const {name, home, dislikes} = this.state
+   
+//   return <h1>{`{name} lives in {home}. He really loaths {dislikes}</h1>
+//   }
+// }
+ 
 
 // 4. Write a React method that would add one and update the state of the count each time the method is called.
 
-this.state = {
-  count: 0
-}
+// this.state = {
+//   count: 0
+// }
 
+// addOne = () => {
+//   this.setState({ count: this.state.count + 1})
+//   }
+  
+// <button onClick={this.addOne}>Add One</button>
 
 
 // 5. There are four mistakes in this code that would cause it to break our application. Find the mistakes and fix them:
 
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class Recipes{
-  constructor(props){
-    super(props)
-    this.state = {
-      recipes:
-        {name: 'Meatballs'},
-        {name: 'Mac & Cheese'}
-    }
-  }
+// class Recipes extends Component{ 
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       recipes: [{name: 'Meatballs'}, {name: 'Mac & Cheese'}]
+//     }
+//   }
 
-  render() {
-    return(
-      let recipe = recipes.map(recipe => {
-        return(
-          <li key={recipe.name}>{recipe.name}</li>
-        )
-      })
-      <ul>
-        {recipe}
-      </ul>
-    )
-  }
-}
-export default Recipes
+//   render() {
+      // let {recipies} = this.state
+      // let recipe = recipes.map(recipe => {
+//         return(
+//           <li key={recipe.name}>{recipe.name}</li>
+//         )
+//       })
+//     return(
+//       <ul>
+//         {recipe}
+//       </ul>
+//     )
+//   }
+// }
+// export default Recipes
